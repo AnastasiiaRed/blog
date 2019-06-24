ba95e4d49dcbe2c6dfd73ad3f3e03428bde44127
AnastasiiaRed <nika23041997@gmail.com>
2019-06-24 23:39:08 +0300

    'blog'

--- blog/blog_page/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import Comments
-
-class CommentForm(forms.ModelForm):
-
-    class Meta:
-        model = Comments
-        fields = ( 'comments_text',)
--- blog/blog_page/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from blog_page import views
- 
-urlpatterns = [
-    path('addcomment/', views.addcomment)
-]
